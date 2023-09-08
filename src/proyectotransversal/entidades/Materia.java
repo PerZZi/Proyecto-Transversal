77b5b8f73d267a7c9ec4be310db0fe861d0dc799
seba7878 <seba1978@hotmail.com>
2023-09-08 10:37:10 -0300

    Update Materia.java

--- src/proyectotransversal/entidades/Materia.java
+++ src/proyectotransversal/entidades/Materia.java
@@ -65,7 +65,7 @@ public class Materia {
 
     @Override
     public String toString() {
-        return "Materia{" + "idMateria=" + idMateria + ", nombre=" + nombre + ", a\u00 f1o=" + año + '}';
+        return "Materia{" + "idMateria=" + idMateria + ", nombre=" + nombre + ", año=" + año + '}';
     }
     
     
