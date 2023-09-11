5165f4833081730fed9b8623d0ae769d52ba4235
seba7878 <seba1978@hotmail.com>
2023-09-11 11:19:50 -0300

    Actualizacion Alumno

--- src/proyectotransversal/AccesoDatos/AlumnoData.java
+++ src/proyectotransversal/AccesoDatos/AlumnoData.java
@@ -6,6 +6,12 @@
 package proyectotransversal.AccesoDatos;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
 import proyectotransversal.entidades.Alumno;
 
 /**
@@ -13,16 +19,34 @@ import proyectotransversal.entidades.Alumno;
  * @author Usuario
  */
 public class AlumnoData {
-    
-    private Connection con=null;
-    
-    public AlumnoData(){
-        
-        con=Conexion.getConnection();
+
+    private Connection con = null;
+
+    public AlumnoData() {
+
+        con = Conexion.getConnection();
     }
-    
-    
-    public void guardarAlumno(Alumno alumno){
-        
+
+    public void guardarAlumno(Alumno alumno) {
+
+        String sql = "INSERT INTO alumno (dni, apellido, nombre, fechaNaci, activo) VALUES (?, ?, ?, , ?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, alumno.getDni());
+            ps.setString(2, alumno.getApellido());
+            ps.setString(3, alumno.getNombre());
+            ps.setDate(4, Date.valueOf(alumno.getFechaNaci()));
+            ps.setBoolean(5, alumno.isActivo());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                alumno.setIdAlumno(rs.getInt("idAlumno"));
+                JOptionPane.showMessageDialog(null, "Alumno añadido con exito");
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Alumno" + ex.getMessage());
+        }
     }
 }
