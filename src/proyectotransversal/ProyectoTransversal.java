a525c296c7d6aee27613a6fe6ddcec5585983a52
seba7878 <seba1978@hotmail.com>
2023-09-12 10:37:49 -0300

    Update ProyectoTransversal.java

--- src/proyectotransversal/ProyectoTransversal.java
+++ src/proyectotransversal/ProyectoTransversal.java
@@ -12,11 +12,13 @@ import proyectotransversal.AccesoDatos.AlumnoData;
 import proyectotransversal.AccesoDatos.Conexion;
 import proyectotransversal.entidades.Alumno;
 
+
 /**
  *
  * @author Usuario
  */
 public class ProyectoTransversal {
+    
 
     /**
      * @param args the command line arguments
