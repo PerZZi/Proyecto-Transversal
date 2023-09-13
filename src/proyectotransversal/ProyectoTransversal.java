33ef92ce870df31f6b9b8524d55a5cc40bfcbddd
PerZZi <emiliano.estebandiaz@gmail.com>
2023-09-13 10:51:49 -0300

    Metodo modificar

--- src/proyectotransversal/ProyectoTransversal.java
+++ src/proyectotransversal/ProyectoTransversal.java
@@ -11,30 +11,20 @@ import proyectotransversal.AccesoDatos.AlumnoData;
 import proyectotransversal.AccesoDatos.Conexion;
 import proyectotransversal.entidades.Alumno;
 
-/**
- *
- * @author Usuario
- */
 public class ProyectoTransversal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         
         Connection con=Conexion.getConnection();
         
-        Alumno alumno1=new Alumno(4578565,"Suarez","Martin",LocalDate.of(2001, 7, 15),true);
+        Alumno alumno1=new Alumno(18, 4296545,"Fernandez","Mateo",LocalDate.of(2000, 8, 20),true);
+        Alumno alumno2=new Alumno(15, 4569874,"Dorregaray","Lucio",LocalDate.of(2003, 6, 10),true);
         AlumnoData alu1=new AlumnoData();
-        alu1.guardarAlumno(alumno1);
-
-
-
-
-
-
-
+        AlumnoData alu2=new AlumnoData();
+        //alu1.guardarAlumno(alumno1);
+        //alu2.guardarAlumno(alumno2);
+        //alu1.modificarAlumno(alumno1);
+        alu2.modificarAlumno(alumno2);
 
     }
-    
 }
